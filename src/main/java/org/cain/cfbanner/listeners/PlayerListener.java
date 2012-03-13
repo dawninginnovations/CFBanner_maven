cbe55758802dfbbf1279aa3171cabd87e3d5aae6
frelling <frelling@curler.com>
2012-03-13 08:30:51 -0400

    Wrong event (login) was used in v2.0, should be join event otherwise permissions are not available, effectively disabling everyone even those with the proper permissiosn.

--- src/main/java/org/cain/cfbanner/listeners/PlayerListener.java
+++ src/main/java/org/cain/cfbanner/listeners/PlayerListener.java
@@ -2,7 +2,7 @@ package org.cain.cfbanner.listeners;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.*;
 
 public class PlayerListener implements Listener {
 	
@@ -11,7 +11,7 @@ public class PlayerListener implements Listener {
 	String ZOMBE_NOCLIP = "§f §f §4 §0 §9 §6";
 	
 	@EventHandler( priority = EventPriority.NORMAL )
-	public void onPlayerLogin(PlayerLoginEvent e) {
+	public void onPlayerJoin(PlayerJoinEvent e) {
 		Player p = e.getPlayer();
 		if(!p.hasPermission("cfbanner.zombe.fly")) {
 			p.sendMessage(ZOMBE_FLY);
